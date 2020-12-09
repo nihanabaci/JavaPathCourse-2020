9505c9eba1508fd0c810f69e8565504286eb7a64
Nihan Abaci <nihanabaci@Nihans-Air.home>
2020-12-08 19:58:56 -0500

    Adding the code for arrays

---
+++ Algorithms-Hackkerank/Arrays/Arrays_left_rotation.java
@@ -0,0 +1,67 @@
+﻿import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the rotLeft function below.
+    static int[] rotLeft(int[] a, int d) {
+        int[] rotatedArray = new int[a.length];
+        for(int i=0; i<a.length; i++){ //a = 1,2,3,4,5, d=2
+            int newIDX = 0;
+            if(i - d < 0){ //i=4 - 2
+                newIDX = i - d + a.length; // 1 - 2 + 5 = 4
+            }
+            else{
+                newIDX = i - d; //4 - 2 = 2
+            }
+            rotatedArray[newIDX] = a[i]; // 3 4 5 1 2
+            
+        }
+        return rotatedArray;
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nd = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nd[0]);
+
+        int d = Integer.parseInt(nd[1]);
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        int[] result = rotLeft(a, d);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
